dafd6bb4fa8765280731fb11dfd8685709ccd094
GetSwastik <Getswastik@gmail.com>
2020-04-26 12:03:35 +0530

    podcast changes

--- app/User.php
+++ app/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable implements MustVerifyEmail
     }
 
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'email_verified_at', 'provider', 'image', 'provider_id'
     ];
 
     protected $hidden = [
