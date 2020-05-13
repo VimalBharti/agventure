5b1072c27a2b2d214535bf8c71bc68d2e493d761
GetSwastik <Getswastik@gmail.com>
2020-05-13 20:12:52 +0530

    service worker new

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,17 +2,25 @@
 
 @section('content')
     
-<div class="main-container">
-    <v-container>
-        <v-row class="app-post-home-page">  
-            <v-col md="8">
-                @include('posts.all')
-            </v-col>
-            
-            <!-- right Sidebar -->
-            @include('_partials.rightSidebar')
-        </v-row>
-    </v-container>
+<div class="main-container boxed-layout">
+
+    <v-row class="app-post-home-page">  
+        <v-col cols="8">
+            @include('posts.all')
+        </v-col>
+        
+        <!-- right Sidebar -->
+        @include('_partials.rightSidebar')
+    </v-row>
+</div>
+
+<div class="mobile-container">
+    <!-- App post Mobile screen -->
+    <div class="app-post-mobile-page">
+        @include('posts.allMobile')
+    </div>
+    <!-- footer link bar -->
+    @include('mobile.footer')
 </div>
 
 @endsection
